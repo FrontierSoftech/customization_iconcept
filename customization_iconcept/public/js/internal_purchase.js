55798df7cc883561538fc6fea92bb016d18a0e9a
Frontier Softech <frontier.updates@gmail.com>
2026-02-04 11:01:48 +0530

    changes in internal transfer

--- customization_iconcept/public/js/internal_purchase.js
+++ customization_iconcept/public/js/internal_purchase.js
@@ -1,83 +1,89 @@
+// frappe.ui.form.on("Purchase Invoice", {
+// 	refresh(frm) {
+// 		if (frm.is_new()) {
+// 			frm.add_custom_button(
+// 				__("Internal Transfer"),
+// 				() => {
+// 					erpnext.utils.map_current_doc({
+// 						method: "customization_iconcept.internal_purchase.make_internal_transfer_sales_invoice",
+// 						source_doctype: "Sales Invoice",
+// 						target: frm,
+// 						setters: {
+// 							customer: undefined,
+// 							posting_date: undefined,
+// 							company: frm.doc.company,
+// 						},
+// 						get_query_filters: {
+// 							docstatus: 1,
+// 							is_internal_customer: 1,
+// 							is_return: 0,
+// 							company: frm.doc.company,
+// 						},
+// 						allow_child_item_selection: true,
+// 						child_fieldname: "items",
+// 						child_columns: [
+// 							"item_code",
+// 							"item_name",
+// 							"qty",
+// 							"rate",
+// 							"amount",
+// 							"warehouse",
+// 						],
+// 					});
+// 				},
+// 				__("Get Items From")
+// 			);
+// 		}
+// 	},
+// });
+
 frappe.ui.form.on("Purchase Invoice", {
 	refresh(frm) {
-		if (frm.is_new()) {
-			frm.add_custom_button(
-				__("Internal Transfer"),
-				() => {
-					erpnext.utils.map_current_doc({
-						method: "customization_iconcept.internal_purchase.make_internal_transfer_sales_invoice",
-						source_doctype: "Sales Invoice",
-						target: frm,
-						setters: {
+		if (!frm.is_new()) return;
+
+		frm.add_custom_button(
+			__("Internal Transfer"),
+			async () => {
+				// Fetch already transferred Sales Invoices
+				const r = await frappe.call({
+					method: "customization_iconcept.internal_purchase.get_sales_invoices_already_transferred",
+					args: {
+						company: frm.doc.company,
+					},
+				});
+				
+				const excluded_sales_invoices = r.message || [];
+
+				erpnext.utils.map_current_doc({
+					method: "customization_iconcept.internal_purchase.make_internal_transfer_sales_invoice",
+					source_doctype: "Sales Invoice",
+					target: frm,
+					setters: {
 							customer: undefined,
 							posting_date: undefined,
 							company: frm.doc.company,
-						},
-						get_query_filters: {
-							docstatus: 1,
-							is_internal_customer: 1,
-							is_return: 0,
-							company: frm.doc.company,
-						},
-						allow_child_item_selection: true,
-						child_fieldname: "items",
-						child_columns: [
-							"item_code",
-							"item_name",
-							"qty",
-							"rate",
-							"amount",
-							"warehouse",
-						],
-					});
-				},
-				__("Get Items From")
-			);
-		}
+					},
+					get_query_filters: {
+						docstatus: 1,
+						is_internal_customer: 1,
+						is_return: 0,
+						company: frm.doc.company,
+						name: ["not in", excluded_sales_invoices || []],
+					},
+					allow_child_item_selection: true,
+					child_fieldname: "items",
+					child_columns: [
+						"item_code",
+						"item_name",
+						"qty",
+						"rate",
+						"amount",
+						"warehouse",
+					],
+				});
+			},
+			__("Get Items From")
+		);
 	},
+	
 });
-
-// frappe.ui.form.on("Purchase Invoice", {
-// 	refresh(frm) {
-// 		if (!frm.is_new()) return;
-
-// 		frm.add_custom_button(
-// 			__("Internal Transfer"),
-// 			async () => {
-// 				// Fetch already transferred Sales Invoices
-// 				const r = await frappe.call({
-// 					method: "customization_iconcept.internal_purchase.get_sales_invoices_already_transferred",
-// 					args: {
-// 						company: frm.doc.company,
-// 					},
-// 				});
-
-// 				const excluded_sales_invoices = r.message || [];
-
-// 				erpnext.utils.map_current_doc({
-// 					method: "customization_iconcept.internal_purchase.make_internal_transfer_sales_invoice",
-// 					source_doctype: "Sales Invoice",
-// 					target: frm,
-// 					get_query_filters: {
-// 						docstatus: 1,
-// 						is_internal_customer: 1,
-// 						is_return: 0,
-// 						company: frm.doc.company,
-// 						name: ["not in", excluded_sales_invoices],
-// 					},
-// 					allow_child_item_selection: true,
-// 					child_fieldname: "items",
-// 					child_columns: [
-// 						"item_code",
-// 						"item_name",
-// 						"qty",
-// 						"rate",
-// 						"amount",
-// 						"warehouse",
-// 					],
-// 				});
-// 			},
-// 			__("Get Items From")
-// 		);
-// 	},
-// });
