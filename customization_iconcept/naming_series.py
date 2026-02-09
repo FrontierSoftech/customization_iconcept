88c040a74dea53f061bd65fed531c80bd931bc78
Frontier Softech <frontier.updates@gmail.com>
2026-02-09 13:14:24 +0530

    changes in naming series

--- customization_iconcept/naming_series.py
+++ customization_iconcept/naming_series.py
@@ -13,41 +13,41 @@ def before_insert(doc, method):
 
     if doc.custom_credit_note:
         # doc.custom_vch_abbr = "CN"
-        doc.naming_series = f"{start_year}CN-.{doc.custom_branch_code}./.#"
-        doc.name = f"{start_year}CN-.{doc.custom_branch_code}./.#"
+        doc.naming_series = f"{start_year}CN-{doc.custom_branch_code}/.#"
+        doc.name = f"{start_year}CN-{doc.custom_branch_code}/.#"
 
     elif doc.custom_is_stock_transfer:
         # doc.custom_vch_abbr = "SR"
-        doc.naming_series = f"STRF-.{doc.custom_branch_code}.-.#"
-        doc.name = f"STRF-.{doc.custom_branch_code}.-.#"
+        doc.naming_series = f"STRF-{doc.custom_branch_code}-.#"
+        doc.name = f"STRF-{doc.custom_branch_code}-.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./SR-{start_year}/.#")
     elif doc.is_return:
         # doc.custom_vch_abbr = "SR"
-        doc.naming_series = f"{start_year}SRN-.{doc.custom_branch_code}./.#"
-        doc.name = f"{start_year}SRN-.{doc.custom_branch_code}./.#"
+        doc.naming_series = f"{start_year}SRN-{doc.custom_branch_code}/.#"
+        doc.name = f"{start_year}SRN-{doc.custom_branch_code}/.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./SR-{start_year}/.#")
        
     elif internal_customer:
         # doc.custom_vch_abbr = "IC"
-        doc.naming_series = f".{company_abbr}{doc.custom_branch_code}.-SO-{start_year}/.#"
-        doc.name = f".{company_abbr}{doc.custom_branch_code}.-SO-{start_year}/.#"
+        doc.naming_series = f"{company_abbr}{doc.custom_branch_code}-SO-{start_year}/.#"
+        doc.name = f"{company_abbr}{doc.custom_branch_code}-SO-{start_year}/.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./IC-{start_year}/.#")    
     else:
         # doc.custom_vch_abbr = "S"
-        doc.naming_series = f".{company_abbr}.-.{doc.custom_branch_code}./S-{start_year}/.#"
-        doc.name = f".{company_abbr}.-.{doc.custom_branch_code}./S-{start_year}/.#"
+        doc.naming_series = f"{company_abbr}-{doc.custom_branch_code}/S-{start_year}/.#"
+        doc.name = f"{company_abbr}-{doc.custom_branch_code}/S-{start_year}/.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./S-{start_year}/.#")
 
 def naming_series_sales_order(doc, method):
     company_abbr = doc.get("custom_company_abbr")[:2]
-    doc.naming_series = f".{company_abbr}.-.{doc.custom_branch_code}./SO-{start_year}/.#"
-    doc.name = f".{company_abbr}.-.{doc.custom_branch_code}./SO-{start_year}/.#"
+    doc.naming_series = f"{company_abbr}-{doc.custom_branch_code}/SO-{start_year}/.#"
+    doc.name = f"{company_abbr}-{doc.custom_branch_code}/SO-{start_year}/.#"
     # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./SO-{start_year}/.#")
 
 def naming_series_delivery_note(doc, method):
     company_abbr = doc.get("custom_company_abbr")[:2]
-    doc.naming_series = f".{company_abbr}.-.{doc.custom_branch_code}./DN-{start_year}/.#"
-    doc.name = f".{company_abbr}.-.{doc.custom_branch_code}./DN-{start_year}/.#"
+    doc.naming_series = f"{company_abbr}-{doc.custom_branch_code}/DN-{start_year}/.#"
+    doc.name = f"{company_abbr}-{doc.custom_branch_code}/DN-{start_year}/.#"
     # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./DN-{start_year}/.#")
 
 def naming_series_purchase_invoice(doc, method):
@@ -55,57 +55,57 @@ def naming_series_purchase_invoice(doc, method):
     company_abbr = doc.get("custom_company_abbr")[:2]
     internal_customer = frappe.db.get_value("Supplier",doc.supplier,"is_internal_supplier")
     if doc.custom_debit_note:
-        doc.naming_series = f"{start_year}DN-.{doc.custom_branch_code}./.#"
-        doc.name = f"{start_year}DN-.{doc.custom_branch_code}./.#"
+        doc.naming_series = f"{start_year}DN-{doc.custom_branch_code}/.#"
+        doc.name = f"{start_year}DN-{doc.custom_branch_code}/.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./PDN-{start_year}/.#")
     elif doc.is_return:
-        doc.naming_series = f"{start_year}P.RET-{doc.custom_branch_code}/.#"
-        doc.name = f"{start_year}P..RET-{doc.custom_branch_code}/.#"
+        doc.naming_series = f"{start_year}PRET-{doc.custom_branch_code}/.#"
+        doc.name = f"{start_year}PRET-{doc.custom_branch_code}/.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./PR-{start_year}/.#")
     elif internal_customer:
         # doc.custom_vch_abbr = "IC"
-        doc.naming_series = f".{company_abbr}{doc.custom_branch_code}.-SI-{start_year}/.#"
-        doc.name = f".{company_abbr}{doc.custom_branch_code}.-SI-{start_year}/.#"
+        doc.naming_series = f"{company_abbr}{doc.custom_branch_code}-SI-{start_year}/.#"
+        doc.name = f"{company_abbr}{doc.custom_branch_code}-SI-{start_year}/.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./IC-{start_year}/.#") 
     elif doc.custom_is_purchase_expense:
         # doc.custom_vch_abbr = "PE"
-        doc.naming_series = f"{start_year}P.EXP-.{doc.custom_branch_code}./.#"
-        doc.name = f"{start_year}P.EXP-.{doc.custom_branch_code}./.#"
+        doc.naming_series = f"{start_year}PEXP-{doc.custom_branch_code}/.#"
+        doc.name = f"{start_year}PEXP-{doc.custom_branch_code}/.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./PE-{start_year}/.#")      
     else:
-        doc.naming_series = f"{start_year}P-.{doc.custom_branch_code}./.#"
-        doc.name = f"{start_year}P-.{doc.custom_branch_code}./.#"
+        doc.naming_series = f"{start_year}P-{doc.custom_branch_code}/.#"
+        doc.name = f"{start_year}P-{doc.custom_branch_code}/.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./PI-{start_year}/.#")
 
 def naming_series_purchase_order(doc, method):
     if doc.name:
         return
     company_abbr = doc.get("custom_company_abbr")[:2]
-    doc.naming_series = f".{company_abbr}.-.{doc.custom_branch_code}./PO-{start_year}/.#"
-    doc.name = f".{company_abbr}.-.{doc.custom_branch_code}./PO-{start_year}/.#"
+    doc.naming_series = f"{company_abbr}-{doc.custom_branch_code}/PO-{start_year}/.#"
+    doc.name = f"{company_abbr}-{doc.custom_branch_code}/PO-{start_year}/.#"
     # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./PO-{start_year}/.#")
 
 def naming_series_purchase_receipt(doc, method):
     company_abbr = doc.get("custom_company_abbr")[:2]
-    doc.naming_series = f".{company_abbr}.-.{doc.custom_branch_code}./PR-{start_year}/.#"
-    doc.name = f".{company_abbr}.-.{doc.custom_branch_code}./PR-{start_year}/.#"
+    doc.naming_series = f"{company_abbr}-{doc.custom_branch_code}/PR-{start_year}/.#"
+    doc.name = f"{company_abbr}-{doc.custom_branch_code}/PR-{start_year}/.#"
     # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./PR-{start_year}/.#")
 
 def naming_series_payment_entry(doc, method):
     company_abbr = doc.get("custom_company_abbr")[:2]
     if doc.payment_type == "Receive":
-        doc.naming_series = f".{company_abbr}.-.{doc.custom_branch_code}./RC-{start_year}/.#"
-        doc.name = f".{company_abbr}.-.{doc.custom_branch_code}./RC-{start_year}/.#"
+        doc.naming_series = f"{company_abbr}-{doc.custom_branch_code}/RC-{start_year}/.#"
+        doc.name = f"{company_abbr}-{doc.custom_branch_code}/RC-{start_year}/.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./RC-{start_year}/.#")
     else:
-        doc.naming_series = f".{company_abbr}.-.{doc.custom_branch_code}./PY-{start_year}/.#"
-        doc.name = f".{company_abbr}.-.{doc.custom_branch_code}./PY-{start_year}/.#"
+        doc.naming_series = f"{company_abbr}-{doc.custom_branch_code}/PY-{start_year}/.#"
+        doc.name = f"{company_abbr}-{doc.custom_branch_code}/PY-{start_year}/.#"
         # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./PY-{start_year}/.#")
 
 def naming_series_journal_entry(doc, method):
     company_abbr = doc.get("custom_company_abbr")[:2]
-    doc.naming_series = f".{company_abbr}.-.{doc.custom_branch_code}./JE-{start_year}/.#"
-    doc.name = f".{company_abbr}.-.{doc.custom_branch_code}./JE-{start_year}/.#"
+    doc.naming_series = f"{company_abbr}-{doc.custom_branch_code}/JE-{start_year}/.#"
+    doc.name = f"{company_abbr}-{doc.custom_branch_code}/JE-{start_year}/.#"
     # doc.name = make_autoname(f".{doc.custom_company_abbr}.-.{doc.custom_branch_code}./JE-{start_year}/.#")
 
 
