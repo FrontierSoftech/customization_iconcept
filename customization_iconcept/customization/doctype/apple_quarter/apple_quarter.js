dbce96300475ca6ffe04f95af4bd2abd7fd17536
FrontierSoftech <frontier.updates@gmail.com>
2026-02-10 18:12:16 +0530

    add customization

---
+++ customization_iconcept/customization/doctype/apple_quarter/apple_quarter.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Frontier Softech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Apple Quarter", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ customization_iconcept/customization/doctype/item_sub_lob/item_sub_lob_tree.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2026, Frontier Softech and contributors
+// For license information, please see license.txt
+
+// frappe.treeview_settings["Item Sub Lob"] = {
+// };
