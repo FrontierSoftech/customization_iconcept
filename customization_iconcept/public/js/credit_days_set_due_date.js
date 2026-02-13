35451c0b901592f041aaa0299ab049aa8cf03213
Frontier Softech <frontier.updates@gmail.com>
2026-02-13 16:10:48 +0530

    changes

--- customization_iconcept/public/js/credit_days_set_due_date.js
+++ customization_iconcept/public/js/credit_days_set_due_date.js
@@ -1,6 +1,48 @@
+// frappe.ui.form.on('Purchase Invoice', {
+//     bill_date: function(frm) {
+//         calculate_due_date(frm);
+//     },
+
+//     custom_credit_days: function(frm) {
+//         calculate_due_date(frm);
+//     },
+
+//     supplier: function(frm) {
+//         calculate_due_date(frm);
+//     },
+
+//     refresh: function(frm) {
+//         calculate_due_date(frm);
+//     }
+// });
+
+// function calculate_due_date(frm) {
+
+//     if (!frm.doc.bill_date || !frm.doc.custom_credit_days) {
+//         return;
+//     }
+
+//     let credit_days = cint(frm.doc.custom_credit_days);
+
+//     let calculated_due_date = frappe.datetime.add_days(
+//         frm.doc.bill_date,
+//         credit_days
+//     );
+
+//     // Clear payment terms to prevent override
+//     if (frm.doc.payment_terms_template) {
+//         frm.set_value("payment_terms_template", "");
+//     }
+
+//     frm.set_value("due_date", calculated_due_date);
+// }
+
 frappe.ui.form.on('Purchase Invoice', {
+
     bill_date: function(frm) {
-        calculate_due_date(frm);
+        setTimeout(() => {
+            calculate_due_date(frm);
+        }, 300);
     },
 
     custom_credit_days: function(frm) {
@@ -8,7 +50,9 @@ frappe.ui.form.on('Purchase Invoice', {
     },
 
     supplier: function(frm) {
-        calculate_due_date(frm);
+        setTimeout(() => {
+            calculate_due_date(frm);
+        }, 300);
     },
 
     refresh: function(frm) {
@@ -30,9 +74,10 @@ function calculate_due_date(frm) {
     );
 
     // Clear payment terms to prevent override
-    if (frm.doc.payment_terms_template) {
-        frm.set_value("payment_terms_template", "");
-    }
+    frm.set_value("payment_terms_template", "");
 
+    // Force set due_date
     frm.set_value("due_date", calculated_due_date);
+
+    frm.refresh_field("due_date");
 }
